e208ed3f8a5a72318277a9b86f3a9fba648665f4
winch_ <d.winchy@gmail.com>
2017-11-11 19:25:38 +0000

    Added thrust to rocket

---
+++ Assets/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space)) // Can thrust while rotating
+        {
+            Debug.Log("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Turning Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Turning Right");
+        }
+    }
+}
