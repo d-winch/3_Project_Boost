c41e4458dc7e2e9a0b4e1c14a33d9ccc2483a624
winch_ <d.winchy@gmail.com>
2017-11-11 19:55:28 +0000

    Added turning and audio source

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -23,15 +25,25 @@ public class Rocket : MonoBehaviour {
         {
             Debug.Log("Thrusting");
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) // Prevent Layer
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
         {
             Debug.Log("Turning Left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             Debug.Log("Turning Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
